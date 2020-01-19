345928cf1f330ee8d23550e8d303bfdfc954fba1
Sean Swanson <sean-swan@hotmail.com>
2020-01-18 16:00:59 -0800

    Hooked up recipeName and recipeTags fields to be logged on submit. Separating by commas and trimming whitespace on the tags to then print as chips.

--- client/src/app/Recipe/recipe-edit/recipe-edit.component.ts
+++ client/src/app/Recipe/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.scss']
+  selector: "app-recipe-edit",
+  templateUrl: "./recipe-edit.component.html",
+  styleUrls: ["./recipe-edit.component.scss"]
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    console.log(
+      form.value.recipeTags.split(",").map(function(item) {
+        return item.trim();
+      })
+    );
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
